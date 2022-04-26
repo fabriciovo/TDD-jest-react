af0ec6a63d764beca2250687ad2d3a866a34ab27
Fabricio Oliveira <fabricio.v.o@hotmail.com>
2022-04-26 20:13:04 -0300

    Testes funcionando

---
+++ src/App.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("sum", () => {
+
+    const { getByText } = render(<App />) 
+
+    expect(getByText('Hello Word')).toBeTruthy()
+
+});
